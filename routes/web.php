164e97ecf2569d526abd4a9ec79957bab6f51c16
jevguio <jevguio@gmail.com>
2025-12-03 11:41:59 +0800

    login

--- routes/web.php
+++ routes/web.php
@@ -69,8 +69,7 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('/event-availability-status', [EventController::class, 'availability'])->name('event.availability');
     Route::get('/my-schedule', [EventController::class, 'mySchedule'])->name('my.schedule');
-// routes/web.php
-Route::get('/search', [DashboardController::class, 'search'])->name('search');
+// routes/web.php 
 
     Route::get('/performer/availability', [EventController::class, 'performerAvailability'])
         ->name('performer.availability')
