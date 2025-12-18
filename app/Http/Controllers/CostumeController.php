23fdb868dc1e1652c9d3fa29e7b9cb64a2a3c473
jevguio <jevguio@gmail.com>
2025-12-18 11:46:23 +0800

    img

--- app/Http/Controllers/CostumeController.php
+++ app/Http/Controllers/CostumeController.php
@@ -40,6 +40,7 @@ class CostumeController extends Controller
         return response()->json([
             'id' => $costume->id,
             'status' => $costume->status,
+            'img' => $costume->img,
             'date_returned' => $costume->date_returned,
             'date_lost' => $costume->date_lost,
             'date_complied' => $costume->date_complied,
