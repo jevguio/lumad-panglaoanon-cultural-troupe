692ee21520e6469f3c3603915ea82d486b0a1ddd
John Vincent S. Guioguio <125118778+jevguio@users.noreply.github.com>
2026-01-16 11:32:05 +0800

    gg

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,6 +41,8 @@ error_reporting(E_ALL & ~E_DEPRECATED);
     @vite(['resources/css/app.css', 'resources/sass/app.scss', 'resources/js/app.js'])
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    
+  <link rel="icon" type="image/x-icon" href="{{ asset('images/logo.png') }}">
 
 </head>
 
